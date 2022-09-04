195e02c39c88d1a8d79c5af2a5cc857b9304f30d
primolechuga <98437939+primolechuga@users.noreply.github.com>
2022-09-03 23:16:16 -0500

    Python

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,32 +6,36 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = 12
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+            self._asignaturas.append(x)
 
     def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
+        if(len(lista)==0):
             self.listadoAlumnos = [alumno]
+            
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos = lista
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+    def __str__(self):
+        texto="Grupo de estudiantes: "+self._grupo
+        return texto
